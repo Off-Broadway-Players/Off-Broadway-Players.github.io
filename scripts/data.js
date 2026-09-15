5f7ef5ae9c76787e821a257ccf5011ca5f19324d
KathrynMerck <143746130+KathrynMerck@users.noreply.github.com>
2026-09-15 14:44:57 -0400

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,46 +1,46 @@
-$(document).ready(function () {
+$(document).ready(function () { //only does stuff when page is loaded
 
-const tecCats = new Map();
-const castMap = new Map();
-const alphaCastArr = [];
+const CREWLINK = `https://docs.google.com/spreadsheets/d/e/2PACX-1vRvraRhOE86QuO1UlDiSozzWIZuTZdyO02JtphVVvONv9wUfzlnqIM3yJmyOlbPKHDlljH2TLdJ0OlQ/pub?gid=416459707&single=true&output=csv`;
+const CASTLINK = `https://docs.google.com/spreadsheets/d/e/2PACX-1vRvraRhOE86QuO1UlDiSozzWIZuTZdyO02JtphVVvONv9wUfzlnqIM3yJmyOlbPKHDlljH2TLdJ0OlQ/pub?gid=0&single=true&output=csv`;
 
-class cast {
+const tecCats = new Map(); //holds the departments of tech and the people under them
+const castMap = new Map(); //holds the cast members, searchable by last name
+const alphaCastArr = []; //holds the cast members' last name for sorting purposes
+
+class cast { //cast [card] class. stores all info, with a method (createCard) to genereate the html for the card
     constructor(i, name, photo, role, bio, p) {
-        this.index = i;
+        this.index = i; //so far unused, but keeping just in case
         this.name = name;
-        this.photo = photo;
+        this.photo = photo; //image url
         let arr = name.split(" ");
-        this.ln = arr.at(-1);
+        this.ln = arr.at(-1); //takes the last word of the name, surname if there are multiple given, first name if no surname given
         this.role = role;
         this.bio = bio;
-        this.p = p;
+        this.p = p; //priority field, if someone should be ordered specially (i.e. not by last name, like the director) their priority should be higher
     }
 
     createCard(){
         let cardDiv = $(`<div></div>`).addClass("card cast-card card-hidden");
         cardDiv.append($("<img>").attr({
-                        "src": "https://www.w3schools.com/howto/img_avatar.png" //add photo functionality
+                        "src": "https://www.w3schools.com/howto/img_avatar.png" //add photo functionality -> "src": this.photo
                     }).addClass("d-block w-100 card-img-top"));
-        // let cfoot = $("<div></div>").addClass("card-footer role").append(this.role);
-        let ctitle = $("<h6></h6>").addClass("card-header card-title text-center").append("<span class=\"name\">" + this.name + "</span><br><span class=\"as\">as</span><br><span class=\"role\">" + this.role + "</span>");
-        let cbody = $("<div></div>").addClass("card-body").append($("<p></p>").append(this.bio.replaceAll(/\\n/g, "<br>").replaceAll(/\\"/g, '"')));
-        cbody.click(expandCard);
+        // let cfoot = $("<div></div>").addClass("card-footer role").append(this.role); //dw about this shhh
+        let ctitle = $("<h6></h6>").addClass("card-header card-title text-center")
+        .append("<span class=\"name\">" + this.name + "</span><br><span class=\"as\">as</span><br><span class=\"role\">" + this.role + "</span>"); //'title' with name and role
+        let cbody = $("<div></div>").addClass("card-body").append($("<p></p>").append(this.bio.replaceAll(/\\n/g, "<br>").replaceAll(/\\"/g, '"'))); //bio, with \n functioning as line break, \" functioning as "
+        cbody.click(expandCard); //enables the "read more" functionality
         let foot = $("<div></div>").addClass("card-footer read-more").append("read more");
-        foot.click(expandCard);
-        cardDiv.append(ctitle, /*cfoot,*/ cbody, foot);
-        // cardDiv.append($('<a></a>').attr({
-        //     "target" : "_blank",
-        //     "href" : this.link
-        // }).addClass("cardLink bi bi-youtube"));
-        let wrapper = $("<div></div>").addClass("col order-" + this.p).append(cardDiv);
+        foot.click(expandCard); //enables the "read more" functionality
+        cardDiv.append(ctitle, cbody, foot);
+        let wrapper = $("<div></div>").addClass("col order-" + this.p).append(cardDiv); //wrapper with priority functionality
         return wrapper;
     }
 }
 
 class crew {
-    constructor(i, cat, name, photo, role, bio, p) {
+    constructor(i, cat, name, photo, role, bio, p) { //overall similar to cast class
         this.index = i;
-        this.cat = cat;
+        this.cat = cat; //holds department, felt cute, might delete later
         this.name = name;
         let arr = name.split(" ");
         this.ln = arr.at(-1);
@@ -51,7 +51,6 @@ class crew {
     }
 
     createCard(){
-        console.log(this.ln);
         let cardDiv = $(`<div></div>`).addClass("card crew-card card-hidden");
         cardDiv.append($("<img>").attr({
                         "src": "https://www.w3schools.com/howto/img_avatar.png"//same deal
@@ -68,26 +67,25 @@ class crew {
     }
 }
 
-let postnum = 0;
+let postnum = 0; //index functionality my beloved
 
-if($("#post-container").hasClass("crew")) {
+if($("#post-container").hasClass("crew")) { //parsing instructions for the crew page
     Papa.parse(
-    `https://docs.google.com/spreadsheets/d/e/2PACX-1vRvraRhOE86QuO1UlDiSozzWIZuTZdyO02JtphVVvONv9wUfzlnqIM3yJmyOlbPKHDlljH2TLdJ0OlQ/pub?gid=416459707&single=true&output=csv`,
+    CREWLINK,
     {
-        download: true,
+        download: true, //look at papaParse docs if interested in config options
         complete: function(results) {
-            for(result of results.data) {
-                const crewItem = new crew(postnum, result.category, result.name, result.photo, result.role, result.bio, result.priority);
-                if(!tecCats.has(result.category)) {
-                    tecCats.set(result.category, [crewItem]);
+            for(result of results.data) { //iterate through the rows of the sheet
+                const crewItem = new crew(postnum, result.category, result.name, result.photo, result.role, result.bio, result.priority); //create crew object
+                if(!tecCats.has(result.category)) { //if category not yet logged
+                    tecCats.set(result.category, [crewItem]); //log it (with an array containing the current crew object)
                 } else {
-                    let temp = tecCats.get(result.category).concat([crewItem]);
-                    tecCats.set(result.category, temp);
+                    let temp = tecCats.get(result.category).concat([crewItem]); //else create a new array with whats there already + current crew object
+                    tecCats.set(result.category, temp); //update the map
                 }
-                // $("#post-container").prepend(shortsItem.createCard());
                 postnum++;
             }
-            crewSetUp($("#post-container"));
+            crewSetUp($("#post-container")); //put it all together
         },
         header: true
     }
@@ -96,18 +94,17 @@ if($("#post-container").hasClass("crew")) {
 
 if($("#post-container").hasClass("cast")) {
     Papa.parse(
-    `https://docs.google.com/spreadsheets/d/e/2PACX-1vRvraRhOE86QuO1UlDiSozzWIZuTZdyO02JtphVVvONv9wUfzlnqIM3yJmyOlbPKHDlljH2TLdJ0OlQ/pub?gid=0&single=true&output=csv`,
+    CASTLINK,
     {
         download: true,
         complete: function(results) {
             for(result of results.data) {
                 const castItem = new cast(postnum, result.name, result.photo, result.role, result.bio, result.priority);
-                castMap.set(castItem.ln, castItem);
-                alphaCastArr.push(castItem.ln);
-                // $("#post-container").prepend(shortsItem.createCard());
+                castMap.set(castItem.ln, castItem); //map shenanagans
+                alphaCastArr.push(castItem.ln); //construct array of last names
                 postnum++;
             }
-            castSetUp($("#post-container"));
+            castSetUp($("#post-container")); //put it all together
         },
         header: true
     }
@@ -116,50 +113,46 @@ if($("#post-container").hasClass("cast")) {
 
 
 function expandCard () { 
-    $(this).parent().toggleClass("card-hidden");
-    if($(this).parent().hasClass("card-hidden")) {
-        if($(this).parent().hasClass("insta-card") || $(this).parent().hasClass("v-card")) {
-            $(this).parent().css("max-height", "450px");
-        } else if ($(this).parent().hasClass("s-card")) {
-            $(this).parent().css("max-height", "600px");
-        }
-        if($(this).hasClass("read-more")) {
-            $(this).css("opacity", 1);
+    $(this).parent().toggleClass("card-hidden"); //toggle hidden state
+    if($(this).parent().hasClass("card-hidden")) { //check for hidden state
+        $(this).parent().css("max-height", "450px"); //might change this later
+        if($(this).hasClass("read-more")) { //if clicked on 'read more'
+            $(this).css("opacity", 1); //make 'read more' text visible 
         } else {
-            $(this).next().css("opacity", 1);
+            $(this).next().css("opacity", 1); 
         }
     } else {
-        $(this).parent().css("max-height", $(this).parent()[0].scrollHeight + "px");
+        $(this).parent().css("max-height", $(this).parent()[0].scrollHeight + "px"); //shows all
         if($(this).hasClass("read-more")) {
-            $(this).css("opacity", 0);
+            $(this).css("opacity", 0); //make read more text invisible
         } else {
-            $(this).next().css("opacity", 0);
+            $(this).next().css("opacity", 0); //make read more text invisible
         }
     }
 }
 
 function crewSetUp (place) {
-    for (const x of tecCats.keys()) {
+    for (const x of tecCats.keys()) { //makes headings for each department
         place.append($("<h3></h3>").addClass("crewCat text-center").append(x));
         let cdiv = $("<div></div>").addClass("row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 pb-4 justify-content-evenly crewDiv CD-" + x);
-        const alphaMap = new Map();
+        const alphaMap = new Map(); //yayy alphabetizing is sooooo fun ._.
         const alphaArr = [];
         for(const y of tecCats.get(x)) {
-            alphaArr.push(y.ln);
-            alphaMap.set(y.ln, y);
+            alphaArr.push(y.ln); //add to surname array
+            alphaMap.set(y.ln, y); 
             // cdiv.append(y.createCard());
         }
-        alphaArr.sort();
+        alphaArr.sort(); //sort surnames alphabetically
         for (const a of alphaArr) {
             let aa = alphaMap.get(a);
-            cdiv.append(aa.createCard());
+            cdiv.append(aa.createCard()); //append in alpha order
         }
-        place.append(cdiv);
+        place.append(cdiv); //bop it right in there
     }
 }
 
 function castSetUp (place) {
-    alphaCastArr.sort();
+    alphaCastArr.sort(); //same as crew but like, easy
     for (const a of alphaCastArr) {
         let aa = castMap.get(a);
         place.append(aa.createCard());
