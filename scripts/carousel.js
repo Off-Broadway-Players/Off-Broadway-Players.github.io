1ead5488a74039f0ef3452372384da0d05670011
KathrynMerck <143746130+KathrynMerck@users.noreply.github.com>
2026-09-15 22:35:13 -0400

    Create carousel.js

---
+++ scripts/carousel.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    Papa.parse(
+        `https://docs.google.com/spreadsheets/d/e/2PACX-1vRvraRhOE86QuO1UlDiSozzWIZuTZdyO02JtphVVvONv9wUfzlnqIM3yJmyOlbPKHDlljH2TLdJ0OlQ/pub?gid=2132180595&single=true&output=csv`,
+        {
+            download: true,
+            complete: function (results) {
+                for(let i = 0; i < results[0].length; i++) {
+                    let div = $("<div></div>").addClass("carousel-item").append($("<img>").attr("src", results[0][i]).addClass("d-block w-100"));
+                    if (i == 0) {
+                        div.addClass("active");
+                    }
+                    $("#c-inner").append(div);
+                }
+            }
+        }
+    );
+});
