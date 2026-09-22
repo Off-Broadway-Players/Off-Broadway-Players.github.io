efe826341f11efe529b1b27c2252942054834b2e
KathrynMerck <143746130+KathrynMerck@users.noreply.github.com>
2026-09-22 18:39:30 -0400

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -130,7 +130,7 @@ function expandCard () {
                 $(this).prev().css("max-height", "10000px");
             }
             // let deg = $(this).css("transform");
-            let deg = $(this).children()[0]//.get(0).style.transform;
+            let deg = $(this).children()[0].get(0).style.transform;
             console.log(deg)
             deg = Number(deg.match(/\d+/)[0]);
             $(this).children("span").css("transform", "rotate(" + (deg + 180) + "deg)"); //make read more text invisible
