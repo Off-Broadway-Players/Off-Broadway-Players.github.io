eba6065472aa0947ad22462097d2515a138e1dad
KathrynMerck <143746130+KathrynMerck@users.noreply.github.com>
2026-09-22 18:28:41 -0400

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -130,7 +130,7 @@ function expandCard () {
                 $(this).prev().css("max-height", "10000px");
             }
             // let deg = $(this).css("transform");
-            let deg = $(this).children("span").get(0).style.transform;
+            let deg = $(this).children()[0].get(0).style.transform;
             console.log(deg)
             deg = Number(deg.match(/\d+/)[0]);
             $(this).children("span").css("transform", "rotate(" + (deg + 180) + "deg)"); //make read more text invisible
@@ -140,10 +140,10 @@ function expandCard () {
             } else {
                 $(this).css("max-height", "10000px");
             }
-            let deg = $(this).next().children("span").get(0).style.transform;
+            let deg = $(this).next().children()[0].get(0).style.transform;
             deg = Number(deg.match(/\d+/)[0]);
             console.log(deg)
-            $(this).next().children("span").css("transform", "rotate(" + (deg + 180) + "deg)"); //make read more text invisible
+            $(this).next().children().css("transform", "rotate(" + (deg + 180) + "deg)"); //make read more text invisible
         }
     // }
 }
