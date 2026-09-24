9bb3829aa4ee9b5c1ad0af1288b5cd437471b70c
KathrynMerck <143746130+KathrynMerck@users.noreply.github.com>
2026-09-24 00:49:15 -0400

    beep

--- scripts/data.js
+++ scripts/data.js
@@ -22,8 +22,8 @@ class cast { //cast [card] class. stores all info, with a method (createCard) to
     createCard(){
         let cardDiv = $(`<div></div>`).addClass("card cast-card card-hidden");
         cardDiv.append($("<img>").attr({
-            "src": "https://www.w3schools.com/howto/img_avatar.png" //add photo functionality -> "src": this.photo
-        }).addClass("d-block w-100 card-img-top"));
+            "src": "./images/frame.svg" //add photo functionality -> "src": this.photo
+        }).addClass("d-block w-100 card-img-top pic"));
         // let cfoot = $("<div></div>").addClass("card-footer role").append(this.role); //dw about this shhh
         let ctitle = $("<h6></h6>").addClass("card-header card-title text-center")
         .append("<span class=\"name\">" + this.name + "</span><br><span class=\"as\">as</span><br><span class=\"role\">" + this.role + "</span>"); //'title' with name and role
@@ -174,10 +174,10 @@ function castSetUp (place) {
         let aa = castMap.get(a);
         place.append(aa.createCard());
     }
-    $(".frameSpan").css({"max-height": $(".pic").height() + 'px'});
+    // $(".frameSpan").css({"max-height": $(".pic").height() + 'px'});
 }
 
 $(window).resize(function () { 
-    $(".frameSpan").css({"max-height": $(".pic").height() + 'px'});
+    // $(".frameSpan").css({"max-height": $(".pic").height() + 'px'});
 });
 });
